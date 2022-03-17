b7ebef863ab29679a95267aea0db72599673e269
sodikfikri <fikrijatim1405@gmail.com>
2022-03-18 06:21:01 +0700

    push to repo

--- movie-database/src/Component/Main.js
+++ movie-database/src/Component/Main.js
@@ -2,10 +2,10 @@ import Hello from "./Hello"
 
 function Main() {
     return(
-        // mengirim props dengan nama 'name'
+        // mengirim parameter props dengan nama 'name'
         <main>
-            <Hello name="Sodik"/>
-            <Hello name="Rapip Nih Bos" />
+            <Hello name="Sodik"/> 
+            <Hello name="Rapip Nih Bos" /> 
             <Hello name="Akira" />
             <Hello name="Narul" />
             <Hello name="Fajar" />
